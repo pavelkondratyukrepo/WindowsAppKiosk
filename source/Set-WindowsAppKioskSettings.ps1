39a1e143de03bf7540f53baf3a7b4ba0641d10d4
Shawn Meyer <shmeyer@microsoft.com>
2025-11-30 13:08:25 -0500

    fixed error in version

--- source/Set-WindowsAppKioskSettings.ps1
+++ source/Set-WindowsAppKioskSettings.ps1
@@ -211,7 +211,7 @@ $EventLog = 'Windows-App-Kiosk'
 $EventSource = 'ConfigScript'
 # Find LTSC OS (and Windows IoT Enterprise)
 $OS = Get-WmiObject -Class Win32_OperatingSystem
-[string]$FullOSVersion = [string]$OS.Version + '.' (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").UBR
+[string]$FullOSVersion = [string]$OS.Version + '.' + (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").UBR
 # Detect Windows 11
 If ($OS.Name -match 'LTSC') { $LTSC = $true }
 # Source Directories and supporting files
